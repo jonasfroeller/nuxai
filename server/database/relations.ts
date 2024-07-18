b58df4351e35a9d0467f56afd7d991f4d1b777bb
Jonas <publik.froeller.jonas@gmx.at>
2024-07-18 13:22:06 +0200

    adds missing relation

--- server/database/relations.ts
+++ server/database/relations.ts
@@ -13,12 +13,17 @@ export const chat_conversation_messageRelations = relations(chat_conversation_me
 }));
 
 export const chat_userRelations = relations(chat_user, ({ many }) => ({
+    chat_conversations: many(chat_conversation),
     chat_conversation_messages: many(chat_conversation_message),
     chat_user_oauth_accounts: many(chat_user_oauth_account),
 }));
 
-export const chat_conversationRelations = relations(chat_conversation, ({ many }) => ({
+export const chat_conversationRelations = relations(chat_conversation, ({ one, many }) => ({
     chat_conversation_messages: many(chat_conversation_message),
+    chat_user: one(chat_user, {
+        fields: [chat_conversation.chat_user_id],
+        references: [chat_user.id]
+    }),
 }));
 
 export const chat_user_oauth_accountRelations = relations(chat_user_oauth_account, ({ one }) => ({
