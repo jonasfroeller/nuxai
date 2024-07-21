1ce28a3bcb52884580797f2a5979743b98057803
Jonas <publik.froeller.jonas@gmx.at>
2024-07-22 01:42:54 +0200

    displays settings if `IS_DEV` is true

--- utils/globals.ts
+++ utils/globals.ts
@@ -1,5 +1,12 @@
 export const IS_DEV = import.meta.dev;
-export const LOG_FRONTEND = Boolean(process.env.LOG_FRONTEND) ?? false;
 export const IS_PRERENDER = import.meta.prerender;
 export const IS_CLIENT = import.meta.client; // import.meta.browser
 export const IS_SERVER = import.meta.server; // import.meta.nitro
+
+export const LOG_FRONTEND = JSON.parse(process.env.LOG_FRONTEND ?? "false") ?? false;
+
+if (IS_DEV) {
+    console.log(
+        `IS_DEV: ${IS_DEV} \nLOG_FRONTEND: ${LOG_FRONTEND} \nIS_PRERENDER: ${IS_PRERENDER} \nIS_CLIENT: ${IS_CLIENT} \nIS_SERVER: ${IS_SERVER}`
+    )
+}
