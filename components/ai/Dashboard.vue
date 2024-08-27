09588592cad88d0f64f4d32260d9480096523c6b
Jonas <publik.froeller.jonas@gmx.at>
2024-08-27 19:50:52 +0200

    fixes chats not rendering, on initial render, because of hydration mismatches

--- components/ai/Dashboard.vue
+++ components/ai/Dashboard.vue
@@ -1,40 +1,77 @@
 <script lang="ts" setup>
-// TODO: fix resizable only working if page is completely rerendered
-
 const { selectedAiChatKey } = useSelectedAiChat();
-const { width } = useWindowSize();
+const isInfoHidden = ref(false);
 </script>
 
 <template>
-  <div>
-    <ShadcnResizablePanelGroup
-      v-if="width >= 1024"
-      id="handle-group-1"
-      direction="horizontal"
-      class="max-w-full mt-2"
-    >
-      <ShadcnResizablePanel
-        id="handle-panel-1"
-        :min-size="25"
-        :default-size="25"
+  <div class="h-full">
+    <ClientOnly>
+      <!-- Panel group for larger screens -->
+      <ShadcnResizablePanelGroup
+        id="handle-group-1"
+        direction="horizontal"
+        class="max-w-full mt-2 panel-group"
       >
-        <AiChatTabs />
-      </ShadcnResizablePanel>
-      <ShadcnResizableHandle id="handle-handle-1" with-handle />
-      <ShadcnResizablePanel
-        id="handle-panel-2"
-        :min-size="50"
-        :default-size="75"
-        class="px-2"
-      >
+        <ShadcnResizablePanel
+          id="handle-panel-1"
+          :class="{ hidden: isInfoHidden }"
+          :min-size="25"
+          :default-size="25"
+        >
+          <AiChatTabs />
+        </ShadcnResizablePanel>
+        <ShadcnResizableHandle
+          id="handle-handle-1"
+          @click="isInfoHidden = !isInfoHidden"
+          with-handle
+        />
+        <ShadcnResizablePanel
+          id="handle-panel-2"
+          :min-size="50"
+          :default-size="75"
+          class="px-2"
+        >
+          <AiChat :key="selectedAiChatKey" />
+          <!-- MESSAGES OF CHAT (needs key, to rerender chat, so that useChat gets a new ID, useChat can not be put anywhere als then the setup script, because some functionality depends on that environment) -->
+        </ShadcnResizablePanel>
+      </ShadcnResizablePanelGroup>
+
+      <!-- Single panel for smaller screens -->
+      <div class="single-panel">
         <AiChat :key="selectedAiChatKey" />
-        <!-- MESSAGES OF CHAT (needs key, to rerender chat, so that useChat gets a new ID, useChat can not be put anywhere als then the setup script, because some functionality depends on that environment) -->
-      </ShadcnResizablePanel>
-    </ShadcnResizablePanelGroup>
-    <div v-else class="pt-2 pr-2">
-      <AiChat :key="selectedAiChatKey" />
-    </div>
+      </div>
+
+      <template #fallback>
+        <div class="pt-2 pr-2">
+          <AiChat :key="selectedAiChatKey" />
+        </div>
+      </template>
+    </ClientOnly>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.panel-group {
+  display: flex;
+}
+
+/* Hide the panel group on screens smaller than 1024px */
+@media (max-width: 1023px) {
+  .panel-group {
+    display: none;
+  }
+}
+
+/* Show the single panel only on screens smaller than 1024px */
+.single-panel {
+  display: none;
+}
+
+@media (max-width: 1023px) {
+  .single-panel {
+    display: block;
+    padding-top: 0.5rem;
+    padding-right: 0.5rem;
+  }
+}
+</style>
